2def1487b7b7ac04214cce985381633883af83b4
Dev Nadson <nadson.pros@gmail.com>
2025-06-23 01:54:31 -0300

    Feat: Javascript Toggle adicionado

--- Projetos-Iniciais/PrimeiroProjeto/script.js
+++ Projetos-Iniciais/PrimeiroProjeto/script.js
@@ -1,3 +1,13 @@
 function toggleMode() {
-    alert("oi")
+    const html = document.documentElement
+    const img = document.querySelector("#profile img") // Mesma tag do css
+
+    html.classList.toggle('light')
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('alt', 'foto perfil light')
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+        img.setAttribute('alt', 'foto perfil dark')
+    }
 }
